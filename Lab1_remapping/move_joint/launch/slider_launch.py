40e8ffdf3200279dd2190e202bb96cec6feef9e4
ioloizou <yiannisloizou@gmail.com>
2024-04-23 17:35:17 +0200

    finished lab 1

--- Lab1_remapping/move_joint/launch/slider_launch.py
+++ Lab1_remapping/move_joint/launch/slider_launch.py
@@ -3,6 +3,16 @@ from simple_launch import SimpleLauncher
 def generate_launch_description():
     sl = SimpleLauncher()
     
-    sl.node('slider_publisher', 'slider_publisher', arguments = [sl.find('move_joint', 'single_joint.yaml')])
+    sl.declare_arg(name='name', default_value='right_e0')
+
+    sl.node(package='slider_publisher', 
+            executable='slider_publisher',
+            name=sl.arg('name'), 
+            arguments = [sl.find('move_joint', 'single_joint.yaml')])
+    
+    sl.node(package='move_joint', 
+            executable= 'move_joint', 
+            parameters={'joint_name': sl.arg('name')}, 
+            remappings={'joint_setpoint': 'setpoint'})
     
     return sl.launch_description() 
